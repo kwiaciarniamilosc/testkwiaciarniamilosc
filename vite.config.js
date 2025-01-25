831736f061dbb95561a5e3afca18d5c758b3b1b6
kwiaciarniamilosc <kwiaciarniamilosc@gmail.com>
2025-01-25 04:02:29 +0000

    Clean up repository

--- vite.config.js
+++ vite.config.js
@@ -9,5 +9,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: '/testkwiaciarniamilosc.pl/', // Update this to match your repository name
+  base: '/testkwiaciarniamilosc.pl/', // Ensure this matches your GitHub Pages repository name
 });
