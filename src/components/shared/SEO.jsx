dae22dff31c25662b864b09c8a5a130c22d24e2b
biuro <biuro@kebabsuperking.com>
2025-02-14 01:27:50 +0000

    Dddd dfdfvrfvr

--- src/components/shared/SEO.jsx
+++ src/components/shared/SEO.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { Helmet } from 'react-helmet-async';
 
 const SEO = ({ title, description, url, image }) => {
+  const defaultTitle = 'Najlepsza Kwiaciarnia w Warszawie - Kwiaciarnia Miłość';
+  const defaultDescription =
+    'Odkryj piękno kwiatów z Kwiaciarnią Miłość - Twoim numerem jeden wśród kwiaciarni w Warszawie. Wyjątkowe bukiety, świeże kwiaty i dostawa na terenie całej Warszawy. Spraw, by każdy moment był niezapomniany!';
+
   return (
     <Helmet>
-      <title>{title ?  : 'Kwiaciarnia Miłość'}</title>
-      <meta name='description' content={description} />
-      <meta name='robots' content='index, follow' />
+      <title>{title ? title : defaultTitle}</title>
+      <meta name="description" content={description ? description : defaultDescription} />
+      <meta name="robots" content="index, follow" />
       <link rel='canonical' href={url} />
       <meta property='og:type' content='website' />
       <meta property='og:title' content={title} />
