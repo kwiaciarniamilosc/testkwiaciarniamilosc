8c041d2dc182704eb06f658d0fe7e46f0f8816ed
biuro <biuro@kebabsuperking.com>
2025-02-14 01:19:39 +0000

    asdffgggg

--- src/components/sections/HeroSection.jsx
+++ src/components/sections/HeroSection.jsx
@@ -164,9 +164,5 @@ import { ChevronRight, ChevronLeft, Heart } from 'lucide-react';
       </header>
     );
   };
-  
-<<<<<<< HEAD
-  export default HeroSection;
-=======
-  export default HeroSection;
->>>>>>> 4b69011498b2ad80e2bc6c9b73dbcb803f5bd939
+
+export default HeroSection;
